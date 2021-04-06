3accdf5ef1f25806d16228a79a4d2f493dbdd3fb
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-04-06 17:40:04 -0400

    creating function to set the icon

---
+++ src/GetIcon.js
@@ -0,0 +1,16 @@
+import day_clear from './weather-icons/day_clear.png'
+import night_full_moon_clear from './weather-icons/night_full_moon_clear.png'
+import angry_clouds from './weather-icons/angry_clouds.png'
+import cloudy from './weather-icons/cloudy.png'
+import day_partial_cloud from './weather-icons/day_partial_cloud.png'
+import overcast from './weather-icons/overcast.png'
+import rain from './weather-icons/rain.png'
+import rain_thunder from './weather-icons/rain_thunder.png'
+import thunder from './weather-icons/thunder.png'
+import snow from './weather-icons/snow.png'
+
+function GetIcon(){
+
+}
+
+export default GetIcon
--- src/weekWeather.js
+++ src/weekWeather.js
@@ -1,15 +1,40 @@
 import day_clear from './weather-icons/day_clear.png';
 import GetLastWeek from './GetLastWeek';
+import moment from 'moment'
+import { useState } from 'react';
+
 var dias_da_semana = ["DOMINGO", "SEGUNDA", "TERÇA", "QUARTA", "QUINTA", "SEXTA", "SÁBADO"];
 
 function WeekWeather(props) {
     var getweat = GetLastWeek();
 
+    var historyDays = moment().subtract(props.id, 'days');
+    var Xmas95 = new Date(historyDays);
+
+    var mes = Xmas95.getMonth();
+    var dia = Xmas95.getDate();
+    var ano = Xmas95.getFullYear();
+    mes++;
+    const [oldTemperature, setOldTemperature] = useState(0);
+
+    async function GetHistoryWeather(city){
+      try{
+          const result = await fetch(`http://api.weatherapi.com/v1/history.json?key=fdb83cfc5a88467a804225243210504&q=${city}&dt=${ano}-${mes}-${dia}`)
+          const data = await result.json();
+          setOldTemperature(data.forecast.forecastday[0].day.maxtemp_c);
+      }catch(error){
+          console.error(error)
+      }
+  }
+
+
+  GetHistoryWeather('Campo Grande');
+
     return (
       <section className="week-Weather-item">
         <h1>{dias_da_semana[getweat[props.id]]}</h1>
         <img src={day_clear}></img>
-        <h1>31°</h1>
+        <h1>{oldTemperature}°</h1>
       </section>
     );
   }
